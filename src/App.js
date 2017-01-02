f0098b702e2fe532e99a0ef8e51b403df7e9c16c
Michael Chen <mzc2fd@virginia.edu>
2017-01-01 19:15:20 -0800

    added comments

--- src/App.js
+++ src/App.js
@@ -14,11 +14,20 @@ let paymentPage = new PaymentPage({
   topContributors: []
 });
 
+/**
+  * @description: handles form submission logic (see submitPayment method for further details)
+*/
 $('#order-form').submit(function(event) {
   event.preventDefault();
   paymentPage.submitPayment();
 });
 
+/**
+  * @description: Whenever an amount-button is clicked, we toggle the selection first to show that it's selected
+  Then, we see if there was a former selection and if there was, we de-select it and set current selection to the button clicked
+  Then, we check if the button was the Custom Amount button or not and set the amount accordingly
+  Lastly, we check to see if the button clicked makes us a top ten contributor and we display accordingly
+*/
 $('.amount-button').click(function() {
   $(this).toggleClass('selected');
   if (paymentPage.selection !== null) {
@@ -36,11 +45,17 @@ $('.amount-button').click(function() {
   paymentPage.checkTopTenContributorDisplay();
 });
 
+/**
+  * @description: handles logic for when "no-thanks" label is clicked, ensuring the display doesn't show again for this submission
+*/
 $('.no-thanks').click(function() {
   paymentPage.noThanks = true;
   paymentPage.checkTopTenContributorDisplay();
 });
 
+/**
+  * @description: parses the custom-amount input and checks to see if the value makes the buyer a top ten contributor
+*/
 $('input[name=custom-amount]').change(function() {
   // regex reference: http://stackoverflow.com/questions/1862130/strip-non-numeric-characters-from-string
   paymentPage.setAmount($(this).val().replace(/[^\d.]/g, '')); // replace globally via `/g` anything that is not a digit or .
@@ -48,6 +63,9 @@ $('input[name=custom-amount]').change(function() {
   paymentPage.checkTopTenContributorDisplay();
 });
 
+/**
+  * @description: stores the name given for the top ten list to be submitted upon "Buy Bundle!"
+*/
 $('input[name=top-ten-name]').change(function() {
   paymentPage.name = $(this).val();
 });
